c66130e7c73c8adf01b613a7852aabdfe9a64f83
Abdul Wahid <babarsahab4u@gmail.com>
2022-10-24 21:31:18 +0500

    app built

---
+++ src/components/ProductFeed/ProductFeed.js
@@ -0,0 +1,59 @@
+import React from "react";
+import ProductItem from "./ProductItem/ProductItem";
+
+function ProductFeed({ products }) {
+  return (
+    <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto">
+      {products.slice(0, 4).map(
+        ({ id, title, price, description, category, image, rating }) => (
+          <ProductItem
+            key={id}
+            id={id}
+            title={title}
+            price={price}
+            description={description}
+            category={category}
+            image={image}
+            rating={rating}
+          />
+        )
+      )}
+
+      <img className="md:col-span-full" src="https://links.papareact.com/dyz" alt="" />
+
+      <div className="md:col-span-2">
+      {products.slice(4, 5).map(
+        ({ id, title, price, description, category, image, rating }) => (
+          <ProductItem
+            key={id}
+            id={id}
+            title={title}
+            price={price}
+            description={description}
+            category={category}
+            image={image}
+            rating={rating}
+          />
+        )
+      )}
+      </div>
+
+      {products.slice(5, products.length).map(
+        ({ id, title, price, description, category, image, rating }) => (
+          <ProductItem
+            key={id}
+            id={id}
+            title={title}
+            price={price}
+            description={description}
+            category={category}
+            image={image}
+            rating={rating}
+          />
+        )
+      )}
+    </div>
+  );
+}
+
+export default ProductFeed;
